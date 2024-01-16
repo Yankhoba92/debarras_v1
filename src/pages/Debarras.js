d34035bd9d4de4a46a9c5d7fc00430f87aed091b
Yankhoba <bagayoko.yan@gmail.com>
2024-01-16 21:58:01 +0100

    🌱 Add Page debarras

---
+++ src/pages/Debarras.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import Navbar from '../components/Navbar';
+import NavbarBurger from '../components/Navbarburger';
+import Error from '../components/Errors';
+
+const meta = {
+  title: '',
+  meta: [],
+  link: [],
+  style: [],
+  script: [],
+};
+
+export default function Debarras() {
+  return (
+    <React.Fragment>
+      <HelmetProvider>
+        <Helmet {...meta}></Helmet>
+      </HelmetProvider>
+      <>
+        <section className='bg-coolGray-900'>
+        <Navbar/>
+        <NavbarBurger/>
+        </section>
+        <Error/>
+      </>
+    </React.Fragment>
+  );
+}
+
